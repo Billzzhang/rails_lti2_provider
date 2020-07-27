c3f397262d7250058b737472eec56076ccddebf1
Billzzhang <billzhang345@gmail.com>
2020-07-27 16:28:46 -0400

    LTI-42: Add rubocop and automated test with test with travis to LTI2Provider gem

--- app/models/rails_lti2_provider/lti_launch.rb
+++ app/models/rails_lti2_provider/lti_launch.rb
@@ -11,16 +11,13 @@ module RailsLti2Provider
     def self.check_launch(lti_message)
       tool = Tool.find_by_uuid(lti_message.oauth_consumer_key)
       raise Unauthorized, :invalid_key unless tool
-      unless IMS::LTI::Services::MessageAuthenticator.new(
-        lti_message.launch_url,
-          lti_message.post_params.merge(lti_message.oauth_params),
-          tool.shared_secret
-      ).valid_signature?
+      unless IMS::LTI::Services::MessageAuthenticator
+             .new(lti_message.launch_url,
+                  lti_message.post_params.merge(lti_message.oauth_params), tool.shared_secret).valid_signature?
         raise Unauthorized, :invalid_signature
       end
-      raise Unauthorized, :invalid_nonce if tool.lti_launches.where(nonce: lti_message.oauth_nonce).positive?
+      raise Unauthorized, :invalid_nonce if tool.lti_launches.where(nonce: lti_message.oauth_nonce).count.positive?
       raise Unauthorized, :request_to_old if DateTime.strptime(lti_message.oauth_timestamp, '%s') < 5.minutes.ago
-
       tool.lti_launches.where('created_at > ?', 1.day.ago).delete_all
       tool.lti_launches.create(nonce: lti_message.oauth_nonce, message: lti_message.post_params)
     end
